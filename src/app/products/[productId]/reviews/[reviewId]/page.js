8eb16afb8b6381f856e688c997024945ac1194ca
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-17 15:40:59 +0500

    error handling using error.js file

--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++ src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,8 +1,16 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt (count) {
+  return Math.floor(Math.random() * count);
+}
+
 const ReviewDetails = ({ params }) => {
   if (parseInt(params.reviewId) > 1000) {
     notFound();
+  }
+  const random = getRandomInt(2);
+  if(random === 1) {
+    throw new Error("Error lodaing review"); 
   }
   return (
     <h1>
