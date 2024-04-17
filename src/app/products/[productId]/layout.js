6da1866a357e357e7e420a1af9fdc612c28dce09
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-17 12:22:34 +0500

    nested layout or layout for specific route

---
+++ src/app/products/[productId]/layout.js
@@ -0,0 +1,10 @@
+export default function ProductDetailsLayout({ children }) {
+    return (
+        <>
+            {children}
+            <h2>
+                Features products
+            </h2>
+        </>
+    )
+}
