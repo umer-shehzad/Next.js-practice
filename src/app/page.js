85e1a8bfb4c11238022837cf4fdc50b81f03978b
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-17 13:10:09 +0500

    link component navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <h1>
-      Welcome Home!
-    </h1>
+    <>
+      <h1>
+        Welcome Home!
+      </h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
   )
 }
 
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 const Products = () => {
     return (
+      <>
+      <Link href="/">Home</Link>
       <h1>
         Products List Page
       </h1>
+      <h3><Link href="/products/1">Product 1</Link></h3>
+      <h3><Link href="/products/2">Product 2</Link></h3>
+      <h3><Link href="/products/3" replace>Product 3</Link></h3>
+      </>
     )
   }
   
