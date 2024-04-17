b8ba95123f5ca980b744b2c5692f4bb4156d39ea
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-17 14:04:20 +0500

    active links

--- src/app/(auth)/(with-auth-layout)/layout.js
+++ src/app/(auth)/(with-auth-layout)/layout.js
@@ -1,10 +1,33 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import './styles.css';
+
+const navLinks = [
+    { name: 'Register', href: "/register" },
+    { name: 'Login', href: "/login" },
+    // { name: 'Froget Password', href: "/forget-password" },
+];
+
 export default function AuthLayout({ children }) {
+    const pathname = usePathname();
     return (
-        <>
-            <h2>
+        <div>
+            {navLinks.map((link) => {
+                const isActive = pathname.startsWith(link.href);
+                return (
+                    <Link href={link.href} key={link.name}
+                        className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+                    >
+                        {link.name}
+                    </Link>
+                )
+            })}
+            {/* <h2>
                 Inner Layout
-            </h2>
+            </h2> */}
             {children}
-        </>
+        </div>
     )
 }
